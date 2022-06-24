d511274d943eca5ec8b37859ca790e1dd1464dff
Ezequiel Amin <ezequielamin@outlook.com>
2022-06-24 10:50:27 -0300

    removed router in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Auth from "./views/Auth";
 import EditPost from "./views/EditPost";
